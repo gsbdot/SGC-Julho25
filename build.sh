1395e02e2afbcedf0f18e845a47bf0b4fb5b6cb5
gsbdot <gsbarbosa90@gmail.com>
2025-07-14 16:13:52 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+# Instala as dependências do Python
 pip install -r requirements.txt
 
+# Executa as migrações do banco de dados para criar/atualizar as tabelas
 flask db upgrade
+
+# Executa nosso novo comando para criar o admin inicial (se não existir)
+flask init-db
